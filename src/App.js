1a908439cbc450f4abec67c4e0c77769632d9b47
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2021-02-02 18:06:48 -0300

    add: routes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
-import React, { useState } from "react";
-import Header from "./components/Header/Header";
-import Footer from "./components/Footer/Footer";
-import { GlobalStyles } from "./helpers/styles/globalStyles";
-import Landing from "./pages/Landing/Landing";
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+// Components
+import Footer from './components/Footer/Footer';
+// Styles
+import { GlobalStyles } from './helpers/styles/globalStyles';
+// Pages
+import Landing from './pages/Landing/Landing';
+import Login from './pages/Login/Login';
 
 export default function App() {
   return (
-    <div>
+    <>
       <GlobalStyles />
 
-      <Header />
-
-      <Landing />
+      <Switch>
+        <Route exact path="/" component={Landing} />
+        <Route path="/login" component={Login} />
+      </Switch>
 
       <Footer />
-    </div>
+    </>
   );
 }
